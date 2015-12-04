fe9223f23d913c6a6d376569049067342500ff0a
Frank Aurich <1100101@gmail.com>
2015-12-04 10:22:50 +0100

    Standalone scripts

---
+++ set_watched.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from resources.lib.myepisodes import MyEpisodes
+
+username = "KyleK"
+password = "CH]NIA3b"
+
+if len(sys.argv) < 2:
+  print "Usage: %s <filename>[ <filename>]" % sys.argv[0]
+  sys.exit(1)
+
+if username is "" or password is "":
+  print "Username or passwor is empty!"
+  sys.exit(1)
+
+mye = MyEpisodes(username, password)
+if not mye.is_logged:
+  print "Login to MyEpisodes.com failed!"
+  sys.exit(1)
+
+if not mye.get_show_list_ex():
+  print "Could not retrieve show list!"
+  sys.exit(1)
+
+for filename in sys.argv[1:]:
+   title, season, episode = MyEpisodes.get_info(filename)
+
+   if title is None or season is None or episode is None:
+     print "Unable to parse file name '%s'. Not a TV show?" % filename
+
+   #print "Parse result: %s (S: %s E: %s)" % (title, season, episode)
+
+   showid = mye.find_show_id(title)
+   if showid is None:
+     print "Show '%s' is not in your personal show list, or does not exist on myepisodes.com. Skipping..." % (title)
+
+   print "Found : %s - %d (%sx%s)'" % (title, showid, season, episode)
+   if not mye.set_episode_watched(showid, season, episode):
+      print "Error setting status for show %d to 'Watched'!" % showid
+
+   print "Successfully changed status for '%s %sx%s' to 'Watched'" % (title, season, episode)
+
+del mye
+sys.exit(0)   
+
---
+++ test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from resources.lib.myepisodes import MyEpisodes
+
+username = "KyleK"
+password = "CH]NIA3b"
+
+if len(sys.argv) is not 2:
+  print "Usage: %s <filename>" % sys.argv[0]
+  sys.exit(1)
+
+if username is "" or password is "":
+  print "Username or passwor is empty!"
+  sys.exit(1)
+
+title, season, episode = MyEpisodes.get_info(sys.argv[1])
+
+if title is None or season is None or episode is None:
+  print "Unable to parse file name '%s'. Not a TV show?" % sys.argv[1]
+  exit(1)
+
+#print "Parse result: %s (S: %s E: %s)" % (title, season, episode)
+
+mye = MyEpisodes(username, password)
+if not mye.is_logged:
+  print "Login to MyEpisodes.com failed!"
+  sys.exit(1)
+
+if not mye.get_show_list_ex():
+  print "Could not retrieve show list!"
+  sys.exit(1)
+
+showid = mye.find_show_id(title)
+if showid is None:
+  print "Show '%s' is not in your personal show list, or does not exist on myepisodes.com. Skipping..." % (title)
+  sys.exit(1)
+
+print "Found : %s S%sE%s (ID: %d)" % (title, season, episode, showid)
+
+del mye
+sys.exit(0)   
